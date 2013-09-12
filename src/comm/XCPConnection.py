23a490da6ed55dd75ff05a6f9e4f4e8d626b7365
Doug Brunner <doug@epicycloidengr.com>
2013-09-12 09:06:06 -0700

    Add tracking of target MTA to eliminate SET_MTA operations, error recovery via resynch, support for program mode including block transfer

--- src/comm/XCPConnection.py
+++ src/comm/XCPConnection.py
@@ -42,6 +42,8 @@ class XCPInvalidOp(XCPError):
 class XCPBadReply(XCPError):
     pass
 
+class XCPPacketLost(XCPError):
+    pass
 
 class XCPConnection(object):
     '''
@@ -67,9 +69,9 @@ class XCPConnection(object):
         self._daq = reply[1] & 0x04
         self._stim = reply[1] & 0x08
         self._pgm = reply[1] & 0x10
-        if not self._calPage:
-            close()
-            raise XCPBadReply()
+        self._pgmStarted = False
+        self._pgmMasterBlockMode = False
+        self._calcMTA = None
         if reply[2] & 0x01:
             self._byteorder = ">"
             self._maxDTO = reply[4] << 8 | reply[5]
@@ -89,18 +91,25 @@ class XCPConnection(object):
         
         self._maxCTO = reply[3]
         
+        self._maxDownloadPayload = self.byteToAG(self._maxCTO - 2) * self._addressGranularity
+        self._maxUploadPayload = self.byteToAG(self._maxCTO - 1) * self._addressGranularity
+        
         if reply[6] != 0x01 or reply[7] != 0x01:
             close()
             raise XCPBadReply()
+    
+    def byteToAG(self, bytes):
+        return int(bytes / self._addressGranularity)
 
     def close(self):
         request = struct.pack(self._byteorder + "B", 0xFE)
+        reply = self._transaction(request, "B")
         if reply[0] != 0xFF:
             raise XCPBadReply()
+        self._pgmStarted = False
     
-    def _transaction(self, request, fmt):
-        self._interface.transmit(request)
-        received = self._interface.receive(self._timeout)
+    def _getReply(self, fmt, timeout):
+        received = self._interface.receive(timeout)
         replies = [rep for rep in received if rep[0] == 0xFF or rep[0] == 0xFE]
         if len(replies) < 1:
             raise XCPTimeout()
@@ -113,6 +122,13 @@ class XCPConnection(object):
         except struct.error:
             raise XCPBadReply()
     
+    def _transaction(self, request, fmt, timeout=-1):
+        self._interface.transmit(request)
+        if timeout < 0:
+            return self._getReply(fmt, self._timeout)
+        else:
+            return self._getReply(fmt, timeout)
+    
     def _synch(self):
         request = struct.pack(self._byteorder + "B", 0xFC)
         reply = self._transaction(request, "BB")
@@ -124,8 +140,8 @@ class XCPConnection(object):
         maxFailures = 2
         while failures <= maxFailures:
             try:
-                return action_func(args) # Jumps out if the action succeeds
-            except XCPTimeout:
+                return action_func(*args) # Jumps out if the action succeeds
+            except (XCPTimeout, XCPPacketLost):
                 failures += 1
                 
             while failures <= maxFailures:
@@ -137,69 +153,116 @@ class XCPConnection(object):
     
     def _setMTA(self, ptr):
         request = struct.pack(self._byteorder + "BBBBL", 0xF6, 1, 0, ptr.ext, ptr.addr)
-        reply = self._transaction(request, "B")
+        try:
+            reply = self._transaction(request, "B")
+        except XCPError:
+            self._calcMTA = None
+            raise
         if reply[0] != 0xFF:
+            self._calcMTA = None
             raise XCPBadReply()
+        self._calcMTA = ptr
     
     def _action_upload8(self, ptr):
         if self._addressGranularity == 1:
             request = struct.pack(self._byteorder + "BBBBL", 0xF4, 1, 0, ptr.ext, ptr.addr)
-            reply = self._transaction(request, "BB")
+            
+            try:
+                reply = self._transaction(request, "BB")
+            except XCPError:
+                self._calcMTA = None
+                raise
+            
             if reply[0] != 0xFF:
+                self._calcMTA = None
                 raise XCPBadReply()
-            return reply[1]
+            else:
+                self._calcMTA = XCPPointer(ptr.addr + 1, ptr.ext)
+                return reply[1]
         else:
             raise XCPInvalidOp()
     
     def upload8(self, ptr):
-        return self._query(_action_upload8, ptr)
+        return self._query(self._action_upload8, ptr)
 
     def _action_upload16(self, ptr):
         if self._addressGranularity == 1:
             request = struct.pack(self._byteorder + "BBBBL", 0xF4, 2, 0, ptr.ext, ptr.addr)
-            reply = self._transaction(request, "BH")
+            decodeFormat = "BH"
         elif self._addressGranularity == 2:
             request = struct.pack(self._byteorder + "BBBBL", 0xF4, 1, 0, ptr.ext, ptr.addr)
-            reply = self._transaction(request, "BxH")
+            decodeFormat = "BxH"
         else:
             raise XCPInvalidOp()
+        
+        try:
+            reply = self._transaction(request, decodeFormat)
+        except XCPError:
+            self._calcMTA = None
+            raise
+            
         if reply[0] != 0xFF:
+            self._calcMTA = None
             raise XCPBadReply()
-        return reply[1]
+        else:
+            self._calcMTA = XCPPointer(ptr.addr + self.byteToAG(2), ptr.ext)
+            return reply[1]
     
     def upload16(self, ptr):
-        return self._query(_action_upload16, ptr)
+        return self._query(self._action_upload16, ptr)
     
     def _action_upload32(self, ptr):
         if self._addressGranularity == 1:
             request = struct.pack(self._byteorder + "BBBBL", 0xF4, 4, 0, ptr.ext, ptr.addr)
-            reply = self._transaction(request, "BL")
+            decodeFormat = "BL"
         elif self._addressGranularity == 2:
             request = struct.pack(self._byteorder + "BBBBL", 0xF4, 2, 0, ptr.ext, ptr.addr)
-            reply = self._transaction(request, "BxL")
+            decodeFormat = "BxL"
         elif self._addressGranularity == 4:
             request = struct.pack(self._byteorder + "BBBBL", 0xF4, 1, 0, ptr.ext, ptr.addr)
-            reply = self._transaction(request, "BxxxL")
+            decodeFormat = "BxxxL"
         else:
             raise XCPInvalidOp()
+        
+        try:
+            reply = self._transaction(request, decodeFormat)
+        except XCPError:
+            self._calcMTA = None
+            raise
+        
         if reply[0] != 0xFF:
+            self._calcMTA = None
             raise XCPBadReply()
-        return reply[1]
+        else:
+            self._calcMTA = XCPPointer(ptr.addr + self.byteToAG(4), ptr.ext)
+            return reply[1]
     
     def upload32(self, ptr):
-        return self._query(_action_upload32, ptr)
+        return self._query(self._action_upload32, ptr)
 
     def _action_download8(self, ptr, data):
         if self._addressGranularity > 1:
             raise XCPInvalidOp()
-        self._setMTA(ptr)
+        if self._calcMTA != ptr:
+            self._setMTA(ptr)
         request = struct.pack(self._byteorder + "BBB", 0xF0, 1, data)
-        reply = self._transaction(request, "B")
+        
+        try:
+            reply = self._transaction(request, "B")
+        except XCPError:
+            self._calcMTA = None
+            raise
+        
         if reply[0] != 0xFF:
+            self._calcMTA = None
             raise XCPBadReply()
+        else:
+            self._calcMTA = XCPPointer(ptr.addr + 1, ptr.ext)
     
     def download8(self, ptr, data):
-        return self._query(_action_download8, ptr, data)
+        if not self._calPage:
+            raise XCPBadReply()
+        return self._query(self._action_download8, ptr, data)
 
     def _action_download16(self, ptr, data):
         if self._addressGranularity == 1:
@@ -208,13 +271,24 @@ class XCPConnection(object):
             request = struct.pack(self._byteorder + "BBH", 0xF0, 1, data)
         else:
             raise XCPInvalidOp()
-        self._setMTA(ptr)
-        reply = self._transaction(request, "B")
+        if self._calcMTA != ptr:
+            self._setMTA(ptr)
+        try:
+            reply = self._transaction(request, "B")
+        except XCPError:
+            self._calcMTA = None
+            raise
+        
         if reply[0] != 0xFF:
+            self._calcMTA = None
             raise XCPBadReply()
+        else:
+            self._calcMTA = XCPPointer(ptr.addr + self.byteToAG(2), ptr.ext)
     
     def download16(self, ptr, data):
-        return self._query(_action_download16, ptr, data)
+        if not self._calPage:
+            raise XCPBadReply()
+        return self._query(self._action_download16, ptr, data)
 
     def _action_download32(self, ptr, data):
         if self._addressGranularity == 1:
@@ -223,13 +297,24 @@ class XCPConnection(object):
             request = struct.pack(self._byteorder + "BBL", 0xF0, 2, data)
         else:
             request = struct.pack(self._byteorder + "BBxxL", 0xF0, 1, data)
-        self._setMTA(ptr)
-        reply = self._transaction(request, "B")
+        if self._calcMTA != ptr:
+            self._setMTA(ptr)
+        try:
+            reply = self._transaction(request, "B")
+        except XCPError:
+            self._calcMTA = None
+            raise
+        
         if reply[0] != 0xFF:
+            self._calcMTA = None
             raise XCPBadReply()
+        else:
+            self._calcMTA = XCPPointer(ptr.addr + self.byteToAG(4), ptr.ext)
     
     def download32(self, ptr, data):
-        return self._query(_action_download32, ptr, data)
+        if not self._calPage:
+            raise XCPBadReply()
+        return self._query(self._action_download32, ptr, data)
     
     def _action_nvwrite(self):
         request = struct.pack(self._byteorder + "BBH", 0xF9, 0x01, 0)
@@ -257,4 +342,177 @@ class XCPConnection(object):
         raise XCPTimeout()
     
     def nvwrite(self):
-        return self._query(_action_nvwrite)
+        if not self._calPage:
+            raise XCPBadReply()
+        return self._query(self._action_nvwrite)
+    
+    def _action_program_start(self):
+        request = struct.pack(self._byteorder + "B", 0xD2)
+        reply = self._transaction(request, "BxBBBBB")
+        if reply[0] != 0xFF or reply[2] < 8:
+            raise XCPBadReply()
+        if reply[1] & 0x01:
+            self._pgmMasterBlockMode = True
+        else:
+            self._pgmMasterBlockMode = False
+        self._pgmMaxCTO = reply[2]
+        self._pgmMaxBS = reply[3]
+        self._pgmMinST = reply[4]
+        self._pgmMaxDownloadPayload = self.byteToAG(self._pgmMaxCTO - 2) * self._addressGranularity
+        self._calcMTA = None
+        self._pgmStarted = True
+    
+    def program_start(self):
+        return self._query(self._action_program_start)
+    
+    def _action_program_clear(self, ptr, length):
+        if self._calcMTA != ptr:
+            self._setMTA(ptr)
+        request = struct.pack(self._byteorder + "BBxxL", 0xD1, 0x00, length)
+        self._calcMTA = None # standard does not define what happens to MTA
+        reply = self._transaction(request, "B", self._nvWriteTimeout)
+        if reply[0] != 0xFF:
+            raise XCPBadReply()
+    
+    def program_clear(self, ptr, length):
+        if not self._pgmStarted:
+            raise XCPInvalidOp()
+        return self._query(self._action_program_clear, ptr, length)
+    
+    def _action_program_block(self, ptr, data):
+        if (len(data) % self._addressGranularity != 0) or (self.byteToAG(len(data)) > self._pgmMaxBS):
+            raise XCPInvalidOp()
+        
+        if self._calcMTA != ptr:
+            self._setMTA(ptr)
+        remBytes = len(data)
+        firstPacket = True
+        
+        while remBytes > 0:
+            # check for a pre-existing error (from a previous operation, or a previous iter of this operation)
+            replies = self._interface.receive(0)
+            # if no replies, everything is OK
+            if len(replies) > 0:
+                self._calcMTA = None
+                # if a reply that is not ERR_SEQUENCE, raise bad reply
+                for reply in replies:
+                    if len(reply) < 2 or reply[0] != 0xFE or reply[1] != 0x29:
+                        raise XCPBadReply() # not ERR_SEQUENCE
+                # if replies are all ERR_SEQUENCE, raise packet lost (and potentially try again)
+                raise XCPPacketLost()
+            
+            if firstPacket:
+                cmdCode = 0xD0
+                firstPacket = False
+            else:
+                cmdCode = 0xCA
+            
+            payloadBytes = min(remBytes, self._pgmMaxDownloadPayload)
+            if self._addressGranularity == 1 or self._addressGranularity == 2:
+                reqHdr = struct.pack(self._byteorder + "BB", cmdCode, self.byteToAG(remBytes))
+            else:
+                reqHdr = struct.pack(self._byteorder + "BBxx", cmdCode, self.byteToAG(remBytes))
+            
+            startOffset = len(data) - remBytes
+            endOffset = startOffset + payloadBytes
+            request = reqHdr + data[startOffset:endOffset]
+            self._interface.transmit(request)
+            remBytes -= payloadBytes
+            self._calcMTA = XCPPointer(self._calcMTA.addr + self.byteToAG(payloadBytes), self._calcMTA.ext)
+        
+        replies = self._interface.receive(self._timeout)
+        if len(replies) == 0:
+            self._calcMTA = None
+            raise XCPTimeout()
+        elif len(replies) > 1 or len(replies[0]) < 1 or replies[0][0] != 0xFF:
+            # some kind of error
+            self._calcMTA = None
+            # if a reply that is not ERR_SEQUENCE, raise bad reply
+            for reply in replies:
+                if len(reply) < 2 or reply[0] != 0xFE or reply[1] != 0x29:
+                    raise XCPBadReply() # not ERR_SEQUENCE
+            # if replies are all ERR_SEQUENCE, raise packet lost (and potentially try again)
+            raise XCPPacketLost()
+    def _program_block(self, ptr, data):
+        return self._query(self._action_program_block, ptr, data)
+    
+    def _action_program_packet(self, ptr, data):
+        if (len(data) % self._addressGranularity != 0) or (len(data) > self._pgmMaxDownloadPayload):
+            raise XCPInvalidOp()
+        
+        if self._calcMTA != ptr:
+            self._setMTA(ptr)
+            
+        if self._addressGranularity == 1 or self._addressGranularity == 2:
+            request = struct.pack(self._byteorder + "BB", 0xD0, len(data)) + data
+        else:
+            request = struct.pack(self._byteorder + "BBxx", 0xD0, len(data)) + data
+        try:
+            reply = self._transaction(request, "B")
+        except XCPError:
+            self._calcMTA = None
+            raise
+        
+        if reply[0] != 0xFF:
+            self._calcMTA = None
+            raise XCPBadReply()
+        else:
+            self._calcMTA = XCPPointer(ptr.addr + self.byteToAG(len(data)), ptr.ext)
+    
+    def _program_packet(self, ptr, data):
+        return self._query(self._action_program_packet, ptr, data)
+    
+    def program_range(self, ptr, data):
+        if not self._pgmStarted:
+            raise XCPInvalidOp()
+        if len(data) % self._addressGranularity != 0:
+            raise XCPInvalidOp()
+        remBytes = len(data)
+        if self._pgmMasterBlockMode:
+            while remBytes > 0:
+                blockBytes = min(remBytes, self._pgmMaxBS * self._addressGranularity)
+                blockStartBytes = len(data) - remBytes
+                blockEndBytes = blockStartBytes + blockBytes
+                blockStartPtr = XCPPointer(ptr.addr + self.byteToAG(blockStartBytes), ptr.ext)
+                self._program_block(blockStartPtr, data[blockStartBytes:blockEndBytes])
+                remBytes -= blockBytes
+        else:
+            while remBytes > 0:
+                packetStartBytes = len(data) - remBytes
+                packetBytes = min(remBytes, self._pgmMaxDownloadPayload)
+                packetEndBytes = packetStartBytes + packetBytes
+                packetStartPtr = XCPPointer(ptr.addr + self.byteToAG(packetStartBytes), ptr.ext)
+                self._program_packet(packetStartPtr, data[packetStartBytes:packetEndBytes])
+                remBytes -= packetBytes
+    
+    def _action_program_verify(self, ptr, crc):
+        request = struct.pack(self._byteorder + "BBHL", 0xC8, 0x01, 0x0002, crc)
+        reply = self._transaction(request, "B")
+        if reply[0] != 0xFF:
+            raise XCPBadReply()
+
+    def program_verify(self, ptr, crc):
+        if not self._pgmStarted:
+            raise XCPInvalidOp()
+        return self._query(self._action_program_verify, ptr, crc)
+        
+    def _action_program_reset(self):
+        request = struct.pack(self._byteorder + "B", 0xCF)
+        reply = self._transaction(request, "B")
+        if reply[0] != 0xFF:
+            raise XCPBadReply()
+        self._pgmStarted = False
+
+    def program_reset(self):
+        if not self._pgmStarted:
+            raise XCPInvalidOp()
+        return self._query(self._action_program_reset)
+        pass
+
+    def program_app(self, ptr, data, crc): # high level function that performs the whole sequence
+        self.program_start()
+        self.program_clear(ptr, len(data))
+        self.program_range(ptr, data)
+        self.program_verify(ptr, crc)
+        self.program_reset()
+        pass
