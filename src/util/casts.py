a55f7a2ac4840a781bba80d4ba0d495f1f602b0c
Doug Brunner <doug@epicycloidengr.com>
2014-07-02 16:39:09 -0700

    Make casts.poly handle casting float input to int

--- src/util/casts.py
+++ src/util/casts.py
@@ -1,9 +1,17 @@
 import struct
 
-structSpecs = {'uint8': 'B', 'int8': 'b', 'uint16': 'H', 'int16': 'h', 'uint32': 'L', 'int32': 'l', 'uint64': 'Q', 'int64': 'q', 'float': 'f', 'double': 'd'}
-
+structSpecs = { \
+    'uint8': 'B', 'int8': 'b', 'uint16': 'H', 'int16': 'h', \
+    'uint32': 'L', 'int32': 'l', 'uint64': 'Q', 'int64': 'q', \
+    'float': 'f', 'double': 'd'\
+}
+pyTypes = { \
+    'uint8': int, 'int8': int, 'uint16': int, 'int16': int, \
+    'uint32': int, 'int32': int, 'uint64': int, 'int64': int, \
+    'float': float, 'double': float\
+}
 def poly(src, dest, i):
-    return struct.unpack_from(structSpecs[src],struct.pack(structSpecs[dest],i))[0]
+    return struct.unpack_from(structSpecs[src],struct.pack(structSpecs[dest],pyTypes[src](i)))[0]
 
 typeSizes = {'uint8': 1, 'int8': 1, 'uint16': 2, 'int16': 2, 'uint32': 4, 'int32': 4, 'uint64': 8, 'int64': 8, 'float': 4, 'double': 8}
 
