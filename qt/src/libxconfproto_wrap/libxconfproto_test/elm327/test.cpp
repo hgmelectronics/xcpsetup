d9bbb0849331f77edad6fe98c47561bdd16c0bf3
Doug Brunner <doug@epicycloidengr.com>
2015-01-02 19:25:40 -0800

    Add transmissions of varying numbers of identical messages to ELM327 echo test
    --HG--
    branch : dev

--- qt/src/libxconfproto_wrap/libxconfproto_test/elm327/test.cpp
+++ qt/src/libxconfproto_wrap/libxconfproto_test/elm327/test.cpp
@@ -41,46 +41,44 @@ void Test::echoTest()
         Id(0x18FF03C0, Id::Type::Ext),
         Id(0x012302F1, Id::Type::Ext)
     };
-
-    for(const IlQByteArray &data : datas)
+    QList<int> nreps =
     {
-        for(const Id &id : ids)
-        {
-            mIntfc->transmitTo(data, id);
-            QList<Frame> rxFrames = mIntfc->receiveFrames(100);
-            qDebug() << mIntfc->elapsedSecs() << "Received" << rxFrames.size() << "frames";
-            if(id.addr % 2 == 0)
-            {
-                QCOMPARE(rxFrames.size(), 1);
-                QCOMPARE(rxFrames[0].id.addr, id.addr + 1);
-                QCOMPARE(rxFrames[0].id.type, id.type);
-                QCOMPARE(rxFrames[0].data, QByteArray(data));
-            }
-            else
-            {
-                QCOMPARE(rxFrames.size(), 0);
-            }
-        }
+        1,
+        2,
+        3,
+        4
+    };
 
-        int nEchoable = 0;
-        for(const Id &id : ids)
+    for(int nrep : nreps)
+    {
+        for(const IlQByteArray &data : datas)
         {
-            mIntfc->transmitTo(data, id);
-            if(id.addr % 2 == 0)
-                ++nEchoable;
-        }
-        QThread::msleep(20);
-        QList<Frame> rxFrames = mIntfc->receiveFrames(100);
-        QCOMPARE(rxFrames.size(), nEchoable);
-        QList<Frame>::iterator rxFrameIt = rxFrames.begin();
-        for(const Id &id : ids)
-        {
-            if(id.addr % 2 == 0)
+            int nEchoable = 0;
+            for(const Id &id : ids)
             {
-                QCOMPARE(rxFrameIt->id.addr, id.addr + 1);
-                QCOMPARE(rxFrameIt->id.type, id.type);
-                QCOMPARE(rxFrameIt->data, QByteArray(data));
-                ++rxFrameIt;
+                for(int i = 0; i < nrep; ++i)
+                {
+                    mIntfc->transmitTo(data, id);
+                    if(id.addr % 2 == 0)
+                        ++nEchoable;
+                }
+            }
+            QThread::msleep(20);
+            QList<Frame> rxFrames = mIntfc->receiveFrames(100);
+            QCOMPARE(rxFrames.size(), nEchoable);
+            QList<Frame>::iterator rxFrameIt = rxFrames.begin();
+            for(const Id &id : ids)
+            {
+                for(int i = 0; i < nrep; ++i)
+                {
+                    if(id.addr % 2 == 0)
+                    {
+                        QCOMPARE(rxFrameIt->id.addr, id.addr + 1);
+                        QCOMPARE(rxFrameIt->id.type, id.type);
+                        QCOMPARE(rxFrameIt->data, QByteArray(data));
+                        ++rxFrameIt;
+                    }
+                }
             }
         }
     }
