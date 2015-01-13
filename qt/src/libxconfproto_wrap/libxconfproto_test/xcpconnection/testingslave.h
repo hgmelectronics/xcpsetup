2bff29fd829c66b66a77817d9d5024c589ef9d11
Doug Brunner <doug@epicycloidengr.com>
2015-01-13 08:29:32 -0800

    Begin declaration of TestingSlave
    --HG--
    branch : dev

--- qt/src/libxconfproto_wrap/libxconfproto_test/xcpconnection/testingslave.h
+++ qt/src/libxconfproto_wrap/libxconfproto_test/xcpconnection/testingslave.h
@@ -7,7 +7,41 @@ class TestingSlave : public QObject
 {
     Q_OBJECT
 public:
-    explicit TestingSlave(QObject *parent = 0);
+    enum class MemType {
+        Calib,
+        Prog
+    };
+    enum class OpType {
+        Connect,
+        Disconnect,
+        GetStatus,
+        Synch,
+        GetCommModeInfo,
+        SetRequest,
+        SetMta,
+        Upload,
+        ShortUpload,
+        TransportLayerCmd,
+        Download,
+        DownloadNext,
+        SetCalPage,
+        GetCalPage,
+        ProgramStart,
+        ProgramClear,
+        Program,
+        ProgramReset,
+        ProgramVerify,
+        ProgramNext,
+        NvWrite
+    };
+    explicit TestingSlave(QObject *parent = 0); // starts thread
+    void SetAG(int ag);
+    void SetMasterBlockSupport(bool enable);
+    int AddMemRange(MemType, size_t base, size_t len);
+    int AddMemRange(MemType, size_t base, std::vector<quint8> data);
+    std::vector<quint8> GetMemRange(int idx);
+    void RemoveMemRange(int idx);
+    void SetResponseDelay(OpType op, int delayMsec, int lifetimeIter);	// delayMsec < 0 means never respond, lifetimeIter < 0 means lasts forever
 
 signals:
 
