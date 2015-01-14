e24b9ef61cf72d981740303eebd88e43ff9c3535
Doug Brunner <doug@epicycloidengr.com>
2015-01-14 15:15:20 -0800

    More TestingSlave
    --HG--
    branch : dev

--- qt/src/libxconfproto_wrap/libxconfproto_test/xcpconnection/testingslave.cpp
+++ qt/src/libxconfproto_wrap/libxconfproto_test/xcpconnection/testingslave.cpp
@@ -1,6 +1,169 @@
 #include "testingslave.h"
 
-TestingSlave::TestingSlave(QObject *parent) :
-    QObject(parent)
+TestingSlave::TestingSlave(SetupTools::Xcp::Interface::Loopback &intfc, QObject *parent = 0) :
+    QThread(parent),
+    mIntfc(intfc),
+    mAg(1),
+    mMaxCto(8),
+    mMaxDto(8),
+    mMasterBlockSupport(false),
+    mNextMemRangeIdx(0),
+    mTerminate(false),
+    mIsConnected(false)
 {
+    start();
+}
+
+TestingSlave::~TestingSlave()
+{
+    mTerminate = true;
+    wait();
+}
+void TestingSlave::setAG(int ag)
+{
+    QMutexLocker locker(&mConfigMutex);
+    Q_ASSERT(ag == 1 || ag == 2 || ag == 4);
+    mAg = ag;
+}
+void TestingSlave::setMaxCto(int bytes)
+{
+    QMutexLocker locker(&mConfigMutex);
+    Q_ASSERT(bytes >= 8);
+    mMaxCto = bytes;
+}
+void TestingSlave::setMaxDto(int bytes)
+{
+    QMutexLocker locker(&mConfigMutex);
+    Q_ASSERT(bytes >= 8);
+    mMaxDto = bytes;
+}
+void TestingSlave::setMasterBlockSupport(bool enable)
+{
+    QMutexLocker locker(&mConfigMutex);
+    mMasterBlockSupport = enable;
+}
+void TestingSlave::setBigEndian(bool isBigEndian)
+{
+    QMutexLocker locker(&mConfigMutex);
+    mIsBigEndian = isBigEndian;
+}
+int TestingSlave::addMemRange(MemType type, SetupTools::Xcp::XcpPtr base, size_t len)
+{
+    QMutexLocker locker(&mConfigMutex);
+    std::vector<quint8> data;
+    data.assign(len, 0);
+    return addMemRange(type, base, data);
+}
+int TestingSlave::addMemRange(MemType type, SetupTools::Xcp::XcpPtr base, std::vector<quint8> data)
+{
+    QMutexLocker locker(&mConfigMutex);
+    int idx = mNextMemRangeIdx;
+    mMemRanges.insert({idx, {type, base, data}});
+    ++mNextMemRangeIdx;
+    return idx;
+}
+const std::vector<quint8> &TestingSlave::getMemRange(int idx)
+{
+    QMutexLocker locker(&mConfigMutex);
+    return mMemRanges[idx];         // will throw if idx doesn't exist
+}
+void TestingSlave::setMemRange(int idx, const std::vector<quint8> &data)
+{
+    QMutexLocker locker(&mConfigMutex);
+    mMemRanges[idx].data = data;    // will throw if idx doesn't exist
+}
+void TestingSlave::removeMemRange(int idx)
+{
+    QMutexLocker locker(&mConfigMutex);
+    std::map<OpType, ResponseDelay>::iterator it = mResponseDelay.find(op);
+    if(it != mResponseDelay.end())
+        mResponseDelay.erase(it);
+}
+void TestingSlave::setResponseDelay(OpType op, int delayMsec, int lifetimeIter)
+{
+    QMutexLocker locker(&mConfigMutex);
+    mResponseDelay.erase(op);
+    mResponseDelay.insert({op, {delayMsec, lifetimeIter}});
+}
+
+void TestingSlave::getResponseDelay(OpType op)
+{
+    std::map<OpType, ResponseDelay>::iterator it = mResponseDelay.find(op);
+    if(it == mResponseDelay.end())
+        return 0;
+    else if(it->second.lifetimeIter <= 0)
+        return 0;
+    else
+    {
+        --(it->second.lifetimeIter);
+        return it->second.msec;
+    }
+}
+bool TestingSlave::isCalPageSupported()
+{
+    for(MemRange &memRange : mMemRanges) {
+        if(memRange.type == MemType::Calib)
+            return true;
+    }
+    return false;
+}
+bool TestingSlave::isPgmSupported()
+{
+    for(MemRange &memRange : mMemRanges) {
+        if(memRange.type == MemType::Pgm)
+            return true;
+    }
+    return false;
+}
+void TestingSlave::transmitWithDelay(OpType op, const std::vector &data)
+{
+    int msec = getResponseDelay(op);
+    if(msec < 0)
+        return;
+    else if(msec > 0)
+        QThread::msleep(msec);
+    mIntfc.slaveTransmit(data);
+}
+
+void TestingSlave::run()
+{
+    while(!mTerminate)
+    {
+        std::vector<std::vector<quint8> > packets = mIntfc.slaveReceive(RECEIVE_TIMEOUT);
+        QMutexLocker locker(&mConfigMutex);
+        for(auto & packet : packets)
+        {
+            if(packet.size() < 1)
+                continue;
+            std::vector<quint8> reply;
+            switch(packet[0])
+            {
+                case 0xFF:
+                    if(packet.size() >= 2 && packet[1] == 0)
+                    {\
+                        reply = {0xFF, 0, 0, 0, 0, 0, 1, 1};
+                        if(isCalPageSupported())
+                            reply[1] |= 0x01;
+                        if(isPgmSupported())
+                            reply[1] |= 0x10;
+                        if(mIsBigEndian)
+                            reply[2] |= 0x01;
+                        if(mAg == 2)
+                            reply[2] |= 0x02;
+                        else if(mAg == 4)
+                            reply[2] |= 0x04;
+                        reply[3] = mMaxCto;
+                        toSlaveEndian(mMaxDto, reply.data() + 4);
+                        mIsConnected = true;
+
+                        transmitWithDelay(OpType::Connect, reply);
+                    }
+                    break;
+                case 0xFE:
+                    mIsConnected = false;
+                    transmitWithDelay(OpType::Disconnect, {0xFF});
+                    break;
+            }
+        }
+    }
 }
--- qt/src/libxconfproto_wrap/libxconfproto_test/xcpconnection/testingslave.h
+++ qt/src/libxconfproto_wrap/libxconfproto_test/xcpconnection/testingslave.h
@@ -2,6 +2,9 @@
 #define TESTINGSLAVE_H
 
 #include <QObject>
+#include <Xcp_Interface_Loopback_Interface.h>
+#include <Xcp_Connection.h>
+#include <map>
 
 class TestingSlave : public QObject
 {
@@ -34,19 +37,97 @@ public:
         ProgramNext,
         NvWrite
     };
-    explicit TestingSlave(QObject *parent = 0); // starts thread
-    void SetAG(int ag);
-    void SetMasterBlockSupport(bool enable);
-    int AddMemRange(MemType, size_t base, size_t len);
-    int AddMemRange(MemType, size_t base, std::vector<quint8> data);
-    std::vector<quint8> GetMemRange(int idx);
-    void RemoveMemRange(int idx);
-    void SetResponseDelay(OpType op, int delayMsec, int lifetimeIter);	// delayMsec < 0 means never respond, lifetimeIter < 0 means lasts forever
 
+    struct ResponseDelay {
+        int msec, iterLeft;
+    };
+
+    struct MemRange {
+        MemType type;
+        SetupTools::Xcp::XcpPtr base;
+        std::vector<quint8> data;
+    };
+
+    explicit TestingSlave(SetupTools::Xcp::Interface::Loopback &intfc, QObject *parent = 0); // starts thread
+    ~TestingSlave();
+    void setAG(int ag);
+    void setMaxCto(int bytes);
+    void setMaxDto(int bytes);
+    void setMasterBlockSupport(bool enable);
+    void setBigEndian(bool isBigEndian);
+    int addMemRange(MemType type, SetupTools::Xcp::XcpPtr base, size_t len);
+    int addMemRange(MemType type, SetupTools::Xcp::XcpPtr base, std::vector<quint8> data);
+    const std::vector<quint8> &getMemRange(int idx);
+    void setMemRange(int idx, const std::vector<quint8> &data);
+    void removeMemRange(int idx);
+    void setResponseDelay(OpType op, int delayMsec, int lifetimeIter);	// delayMsec < 0 means never respond, lifetimeIter < 0 means lasts forever
+
+    template <typename T>
+    T fromSlaveEndian(const uchar *src)
+    {
+        if(mIsBigEndian)
+            return qFromBigEndian<T>(src);
+        else
+            return qFromLittleEndian<T>(src);
+    }
+    template <typename T>
+    T fromSlaveEndian(const char *src)
+    {
+        return fromSlaveEndian(reinterpret_cast<const uchar *>(src));
+    }
+    template <typename T>
+    T fromSlaveEndian(T src)
+    {
+        if(mIsBigEndian)
+            return qFromBigEndian<T>(src);
+        else
+            return qFromLittleEndian<T>(src);
+    }
+    template <typename T>
+    void toSlaveEndian(T src, uchar *dest)
+    {
+        if(mIsBigEndian)
+            qToBigEndian(src, dest);
+        else
+            qToLittleEndian(src, dest);
+    }
+    template <typename T>
+    void toSlaveEndian(T src, char *dest)
+    {
+        toSlaveEndian(src, reinterpret_cast<uchar *>(dest));
+    }
+    template <typename T>
+    T toSlaveEndian(T src)
+    {
+        if(mIsBigEndian)
+            return qToBigEndian<T>(src);
+        else
+            return qToLittleEndian<T>(src);
+    }
 signals:
 
 public slots:
 
+private:
+    void run() Q_DECL_OVERRIDE;
+    int getResponseDelay(OpType op);
+    bool isCalPageSupported();
+    bool isPgmSupported();
+    void transmitWithDelay(OpType op, const std::vector &data);
+
+    static const int RECEIVE_TIMEOUT = 10;
+
+    SetupTools::Xcp::Interface::Loopback &mIntfc;
+    int mAg;
+    quint8 mMaxCto;
+    quint16 mMaxDto;
+    bool mIsBigEndian, mMasterBlockSupport;
+    int mNextMemRangeIdx;
+    std::map<OpType, ResponseDelay> mResponseDelay;
+    std::map<int, MemRange> mMemRanges;
+    QMutex mConfigMutex;
+    bool mTerminate;
+    bool mIsConnected;
 };
 
 #endif // TESTINGSLAVE_H
